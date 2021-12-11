d9c6a93aa78628717f5faebecdfe1179f3fcf672
popatvidhi <popat.v@northeastern.edu>
2021-12-11 16:12:28 -0500

    Provider and Volunteer roles activated to allow switching panels

--- src/Business/Role/ProviderManagerRole.java
+++ src/Business/Role/ProviderManagerRole.java
@@ -9,6 +9,7 @@ import Business.Ecosystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
+import UI.ProviderManager.ProviderManagerWorkArea;
 import javax.swing.JPanel;
 
 /**
@@ -16,17 +17,13 @@ import javax.swing.JPanel;
  * @author vidhi
  */
 public class ProviderManagerRole extends Role{
-    //public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
-        //return new ProviderManagerWorkArea(userProcessContainer/*,account,(SupplierOrganization)organization*/,enterprise,system);
-    //}
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
+        return new ProviderManagerWorkArea(userProcessContainer,enterprise,system);
+    }
         @Override
         public String toString()
 {
    return Role.RoleType.ProviderManagerRole.getValue();
 }
-
-    @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Ecosystem system) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
--- src/Business/Role/VolunteerRole.java
+++ src/Business/Role/VolunteerRole.java
@@ -8,7 +8,9 @@ package Business.Role;
 import Business.Ecosystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
+import Business.Organization.VolunteerOrganization;
 import Business.UserAccount.UserAccount;
+import UI.VolunteerOrganization.DashboardVolunteer;
 import javax.swing.JPanel;
 
 /**
@@ -16,17 +18,14 @@ import javax.swing.JPanel;
  * @author vidhi
  */
 public class VolunteerRole extends Role{
-    //public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
-        //return new VolunteerDashboard(userProcessContainer,account,(VolunteerOrganization)organization,enterprise,system);
-    //}   
+        @Override
+        public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system)
+        {
+            return new DashboardVolunteer(userProcessContainer,account,(VolunteerOrganization)organization,enterprise,system);
+        }   
         @Override
         public String toString()
 {
    return Role.RoleType.VolunteerRole.getValue();
 }
-
-    @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Ecosystem system) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }

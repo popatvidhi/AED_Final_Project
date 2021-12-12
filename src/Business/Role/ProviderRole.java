a92ebd1920b13cf27063ab9da9f3896506e75000
popatvidhi <popat.v@northeastern.edu>
2021-12-11 23:30:51 -0500

    Supply Request functionality added along with bar chart graph

--- src/Business/Role/ProviderRole.java
+++ src/Business/Role/ProviderRole.java
@@ -8,7 +8,9 @@ package Business.Role;
 import Business.Ecosystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
+import Business.Organization.ProviderOrganization;
 import Business.UserAccount.UserAccount;
+import UI.Provider.ProviderWorkArea;
 import javax.swing.JPanel;
 
 /**
@@ -16,17 +18,15 @@ import javax.swing.JPanel;
  * @author vidhi
  */
 public class ProviderRole extends Role{
-    //public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
-        //return new SupplierWorkArea(userProcessContainer,account,(SupplierOrganization)organization,enterprise,system);
-    //}
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
+        return new ProviderWorkArea(userProcessContainer,account,(ProviderOrganization)organization,enterprise,system);
+    }
         @Override
         public String toString()
 {
    return Role.RoleType.ProviderRole.getValue();
 }
 
-    @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Ecosystem system) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+
 }

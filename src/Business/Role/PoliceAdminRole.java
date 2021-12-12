e20c9001ccd086a95e92e86463efd7793ce10559
popatvidhi <popat.v@northeastern.edu>
2021-12-12 03:01:05 -0500

    Police Enterprise created along with functionality

--- src/Business/Role/PoliceAdminRole.java
+++ src/Business/Role/PoliceAdminRole.java
@@ -9,6 +9,7 @@ import Business.Ecosystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
+import UI.PoliceEnterprise.PoliceAdminWorkArea;
 import javax.swing.JPanel;
 
 /**
@@ -16,17 +17,14 @@ import javax.swing.JPanel;
  * @author vidhi
  */
 public class PoliceAdminRole extends Role{
-    //public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
-        //return new PoliceAdminWorkArea(userProcessContainer,/*account,(ChangeMakerOrganization)organization,*/enterprise,system);
-    //}   
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
+        return new PoliceAdminWorkArea(userProcessContainer,enterprise,system);
+    }   
         @Override
         public String toString()
 {
    return Role.RoleType.PoliceAdminRole.getValue();
 }
 
-    @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Ecosystem system) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }

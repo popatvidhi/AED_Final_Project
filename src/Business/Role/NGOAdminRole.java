e4b494b4186067c6b3bff92439462e693f7b800b
popatvidhi <popat.v@northeastern.edu>
2021-12-11 19:58:37 -0500

    NGO Admin role screens and functionality added

--- src/Business/Role/NGOAdminRole.java
+++ src/Business/Role/NGOAdminRole.java
@@ -9,6 +9,7 @@ import Business.Ecosystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
+import UI.NGOEnterprise.NGOAdminWorkArea;
 import javax.swing.JPanel;
 
 /**
@@ -16,18 +17,15 @@ import javax.swing.JPanel;
  * @author vidhi
  */
 public class NGOAdminRole extends Role{
-    //public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
-       // return new NGOAdminWorkArea(userProcessContainer,/*account,(ChangeMakerOrganization)organization,*/enterprise,system);
-    //}   
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,Ecosystem system){
+        return new NGOAdminWorkArea(userProcessContainer,enterprise,system);
+    }   
         @Override
         public String toString()
 {
    return Role.RoleType.NGOAdminRole.getValue();
 }
 
-    @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Ecosystem system) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
   
 }
